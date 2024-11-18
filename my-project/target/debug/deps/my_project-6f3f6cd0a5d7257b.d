ab5680097077e42601f5b4047231e70e43e8ffb2
Mahendra Dani <danimahendra0904@gmail.com>
2024-11-18 23:22:43 +0530

    remove binaries

--- my-project/target/debug/deps/my_project-6f3f6cd0a5d7257b.d
+++
@@ -1,5 +0,0 @@
-/Users/mahendradani/Documents/Programming/Learning/rusty/my-project/target/debug/deps/libmy_project-6f3f6cd0a5d7257b.rmeta: src/main.rs
-
-/Users/mahendradani/Documents/Programming/Learning/rusty/my-project/target/debug/deps/my_project-6f3f6cd0a5d7257b.d: src/main.rs
-
-src/main.rs:
