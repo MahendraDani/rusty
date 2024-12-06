79ea6ea7fd04e4f3d2c1392be18b98f581e0b8b5
Mahendra Dani <danimahendra0904@gmail.com>
2024-12-06 08:13:08 +0530

    remove binaries

--- collections/target/debug/deps/collections-949035bef5dcd0ae.d
+++
@@ -1,5 +0,0 @@
-/Users/mahendradani/Documents/Programming/Learning/rusty/collections/target/debug/deps/libcollections-949035bef5dcd0ae.rmeta: src/main.rs
-
-/Users/mahendradani/Documents/Programming/Learning/rusty/collections/target/debug/deps/collections-949035bef5dcd0ae.d: src/main.rs
-
-src/main.rs:
--- collections/target/debug/deps/collections-bf00f824e83411f3.d
+++
@@ -1,5 +0,0 @@
-/Users/mahendradani/Documents/Programming/Learning/rusty/collections/target/debug/deps/libcollections-bf00f824e83411f3.rmeta: src/main.rs
-
-/Users/mahendradani/Documents/Programming/Learning/rusty/collections/target/debug/deps/collections-bf00f824e83411f3.d: src/main.rs
-
-src/main.rs:
